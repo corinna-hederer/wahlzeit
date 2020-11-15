92cd3c9b9c3c8b323dada97f3c0c1ca9834dcf14
corinna-hederer <74151356+corinna-hederer@users.noreply.github.com>
2020-11-15 20:33:10 +0100

    adap-cw02
    Create Coordinate.java

---
+++ src/main/java/org/wahlzeit/model/Coordinate.java
@@ -0,0 +1,109 @@
+package org.wahlzeit.model;
+
+import java.util.Objects;
+
+/**
+ * A coordinate represents the exact position in a three-dimensional space where a photo was taken
+ */
+
+public class Coordinate {
+
+    private double xCoordinate;
+    private double yCoordinate;
+    private double zCoordinate;
+
+    /**
+     * Creates a coordinate instance using the arguments xCoordinate, yCoordinate and zCoordinate
+     * @methodtype constructor
+     * @param xCoordinate x component of coordinate
+     * @param yCoordinate y component of coordinate
+     * @param zCoordinate z component of coordinate
+     */
+
+    public Coordinate(double xCoordinate, double yCoordinate, double zCoordinate){
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+        this.zCoordinate = zCoordinate;
+    }
+
+
+    /**
+     * Default Coordinate, whose x, y and z components are initialised with 0.0
+     * @methodtype constructor
+     */
+
+    public Coordinate(){
+        this.xCoordinate = 0.0;
+        this.yCoordinate = 0.0;
+        this.zCoordinate = 0.0;
+    }
+
+
+    /**
+     * Getter methods for x, y, and z component
+     * @methodtype get
+     */
+
+    public double getxCoordinate() {
+        return xCoordinate;
+    }
+
+    public double getyCoordinate() {
+        return yCoordinate;
+    }
+
+    public double getzCoordinate() {
+        return zCoordinate;
+    }
+
+
+    /**
+     * Setter methods for x, y, and z component
+     * @methodtype set
+     */
+
+    public double setxCoordinate() {
+        return xCoordinate;
+    }
+
+    public double setyCoordinate() {
+        return yCoordinate;
+    }
+
+    public double setzCoordinate() {
+        return zCoordinate;
+    }
+
+
+    /**
+     * computes the direct cartesian distance of two coordinates
+     * @methodtype calculation
+     * @param c other coordinate to which distance should be calculated
+     * @return distance between the two coordinates
+     */
+
+    public double getDistance(Coordinate c){
+        double distance = Math.sqrt(Math.pow((c.getxCoordinate() - this.xCoordinate), 2) +
+                Math.pow((c.getyCoordinate() - this.yCoordinate), 2) +
+                Math.pow((c.getzCoordinate() - this.zCoordinate), 2) );
+        return distance;
+    }
+
+
+    /**
+     * compares two coordinates in order to determine if they are equal
+     * @methodtype boolean-query
+     * @param c other coordinate with which comparison is made
+     * @return true, if coordinates are equal; false if they are not
+     */
+
+    public boolean isEqual(Coordinate c) {
+        Double x = c.getxCoordinate();
+        Double y = c.getyCoordinate();
+        Double z = c.getzCoordinate();
+        return (x.equals(xCoordinate) && y.equals(yCoordinate) && z.equals(zCoordinate));
+    }
+
+}
+
+
