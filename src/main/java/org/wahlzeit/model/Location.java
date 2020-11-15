f25bd6a9a1776630859308f1bb3c2225b210ddd8
corinna-hederer <74151356+corinna-hederer@users.noreply.github.com>
2020-11-15 20:34:21 +0100

    adap-cw02
    Create Location.java

---
+++ src/main/java/org/wahlzeit/model/Location.java
@@ -0,0 +1,20 @@
+package org.wahlzeit.model;
+
+
+/**
+ * A location represents the coordinates of a photo
+ */
+
+public class Location {
+
+    public Coordinate coordinate;
+
+    /**
+     *
+     * @methodtype constructor
+     */
+    public Location(Coordinate coordinate){
+        this.coordinate=coordinate;
+    }
+
+}
