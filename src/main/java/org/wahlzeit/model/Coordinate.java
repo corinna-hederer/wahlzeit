f16e41cd51113d1f053ee14f17fc99e4eed4bd81
corinna-hederer <74151356+corinna-hederer@users.noreply.github.com>
2020-11-29 13:43:24 +0100

    adap-cw04
    Create Interface Coordinate

--- src/main/java/org/wahlzeit/model/Coordinate.java
+++ src/main/java/org/wahlzeit/model/Coordinate.java
@@ -1,133 +1,30 @@
 package org.wahlzeit.model;
 
-import java.util.Objects;
+public interface Coordinate{
 
-/**
- * A coordinate represents the exact position in a three-dimensional space where a photo was taken
- */
+	/**
+	 * Formats cartesian coordinate as CartesianCoordinate
+	 */
+	public CartesianCoordinate asCartesianCoordinate();
 
-public class Coordinate {
+	/**
+	 * Compute distance between two cartesian coordinates
+	 */
+	public double getCartesianDistance(Coordinate coordinate);
 
-    private double xCoordinate;
-    private double yCoordinate;
-    private double zCoordinate;
+	/**
+	 * Formats spheric coordinate as SphericCoordinate
+	 */
+	public SphericCoordinate asSphericCoordinate();
 
-    /**
-     * Creates a coordinate instance using the arguments xCoordinate, yCoordinate and zCoordinate
-     * @methodtype constructor
-     * @param xCoordinate x component of coordinate
-     * @param yCoordinate y component of coordinate
-     * @param zCoordinate z component of coordinate
-     */
+	/**
+	 * Compute central angle of two spheric coordinates
+	 */
+	public double getCentralAngle(Coordinate coordinate);
 
-    public Coordinate(double xCoordinate, double yCoordinate, double zCoordinate){
-        this.xCoordinate = xCoordinate;
-        this.yCoordinate = yCoordinate;
-        this.zCoordinate = zCoordinate;
-    }
+	/**
+	 * Compares equality of two coordinates
+	 */
+	public boolean isEqual(Coordinate coordinate);
 
-
-    /**
-     * Default Coordinate, whose x, y and z components are initialised with 0.0
-     * @methodtype constructor
-     */
-
-    public Coordinate(){
-        this.xCoordinate = 0.0;
-        this.yCoordinate = 0.0;
-        this.zCoordinate = 0.0;
-    }
-
-
-    /**
-     * Getter methods for x, y, and z component
-     * @methodtype get
-     */
-
-    public double getxCoordinate() {
-        return xCoordinate;
-    }
-
-    public double getyCoordinate() {
-        return yCoordinate;
-    }
-
-    public double getzCoordinate() {
-        return zCoordinate;
-    }
-
-
-    /**
-     * Setter methods for x, y, and z component
-     * @methodtype set
-     */
-
-    public double setxCoordinate() {
-        return xCoordinate;
-    }
-
-    public double setyCoordinate() {
-        return yCoordinate;
-    }
-
-    public double setzCoordinate() {
-        return zCoordinate;
-    }
-
-
-    /**
-     * computes the direct cartesian distance of two coordinates
-     * @methodtype calculation
-     * @param c other coordinate to which distance should be calculated
-     * @return distance between the two coordinates
-     */
-
-    public double getDistance(Coordinate c){
-        double distance = Math.sqrt(Math.pow((c.getxCoordinate() - this.xCoordinate), 2) +
-                Math.pow((c.getyCoordinate() - this.yCoordinate), 2) +
-                Math.pow((c.getzCoordinate() - this.zCoordinate), 2) );
-        return distance;
-    }
-
-
-     /**
-     * compares two coordinates in order to determine if they are equal
-     * @methodtype boolean-query
-     * @param c other coordinate with which comparison is made
-     * @return true, if coordinates are equal; false if they are not
-     */
-
-    public boolean isEqual(Coordinate c) {
-        Double x = c.getxCoordinate();
-        Double y = c.getyCoordinate();
-        Double z = c.getzCoordinate();
-        return (x.compareTo(xCoordinate) == 0 && y.compareTo(yCoordinate) == 0 && z.compareTo(zCoordinate) == 0);
-    }
-
-    /**
-     * Override of equals() to verify if given and current objects are the same
-     * @methodtype comparison
-     * @param o type Object
-     * @return true if object equals current instance
-     */
-
-    @Override
-    public boolean equals(Object o) {
-
-        if (o == this) return true;
-        if(o == null) return false;
-        if (!(o instanceof Coordinate)) {
-            return false;
-        }
-
-        Coordinate c = (Coordinate) o;
-        return isEqual(c);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(xCoordinate, yCoordinate, zCoordinate);
-    }
 }
-
-
