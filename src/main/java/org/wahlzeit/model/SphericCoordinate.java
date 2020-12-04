653248c5ef38bdeff49cfb67816bce52811b0815
corinna-hederer <74151356+corinna-hederer@users.noreply.github.com>
2020-12-04 22:46:26 +0100

    adap-cw05
    Update SphericCoordinate.java,
    extend AbstractCoordinate,
    delete redundant methods

--- src/main/java/org/wahlzeit/model/SphericCoordinate.java
+++ src/main/java/org/wahlzeit/model/SphericCoordinate.java
@@ -60,29 +60,7 @@ public class SphericCoordinate implements Coordinate{
     public void setRadius(double radius) {
         this.radius = radius;
     }
-
-    /**
-     * Override of equals() to verify if given and current objects are the same
-     * @methodtype comparison
-     * @param o type Object
-     * @return true if object equals current instance
-     */
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        SphericCoordinate that = (SphericCoordinate) o;
-        return Double.compare(that.phi, phi) == 0 &&
-                Double.compare(that.theta, theta) == 0 &&
-                Double.compare(that.radius, radius) == 0;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(phi, theta, radius);
-    }
-
+    
 
     /**
      * Implements methods in interface Coordinate
@@ -105,15 +83,6 @@ public class SphericCoordinate implements Coordinate{
 
 
     /**
-     * Transform spheric coordinate into cartesian coordinate and get distance of two cartesian coordinates
-     * @methodtype get
-     */
-    @Override
-    public double getCartesianDistance(Coordinate coordinate) {
-        return this.asCartesianCoordinate().getCartesianDistance(coordinate);
-    }
-
-    /**
      * Interpret coordinate as spheric coordinate
      * @methodtype conversion
      * @return spheric coordinate
@@ -121,36 +90,5 @@ public class SphericCoordinate implements Coordinate{
     @Override
     public SphericCoordinate asSphericCoordinate() {
         return this;
-    }
-
-    /**
-     * Get central angle between two spheric coordinates
-     * @methodtype get
-     * @return centralAngle great circle distance
-     */
-    @Override
-    public double getCentralAngle(Coordinate coordinate) {
-        SphericCoordinate sphericCoordinate = coordinate.asSphericCoordinate();
-        double centralAngle = Math.toDegrees(Math.acos(Math.sin(Math.toRadians(this.phi)) *
-                Math.sin(Math.toRadians(sphericCoordinate.getPhi())) +
-                Math.cos(Math.toRadians(this.phi)) *
-                Math.cos(Math.toRadians(sphericCoordinate.getPhi())) *
-                Math.cos(Math.toRadians(sphericCoordinate.getTheta() - this.theta))));
-        return centralAngle;
-    }
-
-    /**
-     * Compares equality of two coordinates by using a maximal derivation of 0.00001
-     * @methodtype boolean query
-     */
-    @Override
-    public boolean isEqual(Coordinate coordinate) {
-        SphericCoordinate sphericCoordinate = coordinate.asSphericCoordinate();
-        double tolerance = 0.00001;
-        boolean phi_equals = Math.abs(sphericCoordinate.getPhi()-this.phi) <= tolerance;
-        boolean theta_equals = Math.abs(sphericCoordinate.getTheta()-this.theta) <= tolerance;
-        boolean radius_equals = Math.abs(sphericCoordinate.getRadius()-this.radius) <= tolerance;
-        return phi_equals && theta_equals && radius_equals;
-
     }
 }
