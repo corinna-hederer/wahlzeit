03805d5cd3a329f0edcd7db054647ea495cd52fc
corinna-hederer <74151356+corinna-hederer@users.noreply.github.com>
2020-12-21 00:52:07 +0100

    adap-cw07
    Update BuildingPhotoTest

--- src/test/java/org/wahlzeit/model/BuildingPhotoTest.java
+++ src/test/java/org/wahlzeit/model/BuildingPhotoTest.java
@@ -14,7 +14,10 @@ public class BuildingPhotoTest {
 
         Assert.assertEquals(testPhoto.getClass(), BuildingPhoto.class);
     }
+
+    @Test(expected = NullPointerException.class)
+    public void testCreateBuildingPhotoIsNull() throws NullPointerException{
+        Building b = null;
+        BuildingPhoto bp = new BuildingPhoto(b);
+    }
 }
-
-
-
