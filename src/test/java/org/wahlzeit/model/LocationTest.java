d66d08a4fd216828b6c22fc1453eb90cc40eb118
corinna-hederer <74151356+corinna-hederer@users.noreply.github.com>
2021-01-10 23:54:43 +0100

    adap-cw08
    Update LocationTest

--- src/test/java/org/wahlzeit/model/LocationTest.java
+++ src/test/java/org/wahlzeit/model/LocationTest.java
@@ -7,10 +7,9 @@ public class LocationTest {
 
     @Test
     public void instantiateLocation() {
-        Coordinate coordinate = new CartesianCoordinate(1.0, 2.0, 3.0);
+        Coordinate coordinate = CartesianCoordinate.getOrCreateCartesianCoordinate(1.0, 2.0, 3.0);
         Location location = new Location(coordinate);
         Assert.assertNotNull(location);
         Assert.assertEquals(location.coordinate, coordinate);
     }
 }
-
