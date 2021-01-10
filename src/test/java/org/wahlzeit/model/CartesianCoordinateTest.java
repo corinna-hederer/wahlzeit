8eb44daf3868dc19bb2e3f0d78161d5b71c36586
corinna-hederer <74151356+corinna-hederer@users.noreply.github.com>
2021-01-10 23:52:40 +0100

    adap-cw08
    Update CartesianCoordinateTest

--- src/test/java/org/wahlzeit/model/CartesianCoordinateTest.java
+++ src/test/java/org/wahlzeit/model/CartesianCoordinateTest.java
@@ -6,8 +6,8 @@ import org.junit.Test;
 public class CartesianCoordinateTest {
     @Test
     public void testIsEqual() {
-        CartesianCoordinate c1 = new CartesianCoordinate(1.1,2.02, 3.003);
-        CartesianCoordinate c2 = new CartesianCoordinate(1.1,2.02,3.003);
+        CartesianCoordinate c1 = CartesianCoordinate.getOrCreateCartesianCoordinate(1.1,2.02, 3.003);
+        CartesianCoordinate c2 = CartesianCoordinate.getOrCreateCartesianCoordinate(1.1,2.02,3.003);
 
         boolean isEqual = c1.isEqual(c2);
 
@@ -16,8 +16,8 @@ public class CartesianCoordinateTest {
 
     @Test
     public void testIsNotEqual() {
-        CartesianCoordinate c1 = new CartesianCoordinate(2.1,2, 3);
-        CartesianCoordinate c2 = new CartesianCoordinate(1.1,2,3);
+        CartesianCoordinate c1 = CartesianCoordinate.getOrCreateCartesianCoordinate(2.1,2, 3);
+        CartesianCoordinate c2 = CartesianCoordinate.getOrCreateCartesianCoordinate(1.1,2,3);
 
         boolean isEqual = c1.isEqual(c2);
 
@@ -27,8 +27,8 @@ public class CartesianCoordinateTest {
 
     @Test
     public void testGetCartesianDistance(){
-        CartesianCoordinate a = new CartesianCoordinate(1,1, 1);
-        CartesianCoordinate b = new CartesianCoordinate(2,2,2);
+        CartesianCoordinate a = CartesianCoordinate.getOrCreateCartesianCoordinate(1,1, 1);
+        CartesianCoordinate b = CartesianCoordinate.getOrCreateCartesianCoordinate(2,2,2);
 
         double distance = a.getCartesianDistance(b);
         double correctValue = Math.sqrt(3);
@@ -39,32 +39,32 @@ public class CartesianCoordinateTest {
 
     @Test
     public void testAsSphericCoordinate(){
-        CartesianCoordinate a = new CartesianCoordinate(0.0, 1.0, 2.0);
+        CartesianCoordinate a = CartesianCoordinate.getOrCreateCartesianCoordinate(0.0, 1.0, 2.0);
         SphericCoordinate b = a.asSphericCoordinate();
-        SphericCoordinate correctValue = new SphericCoordinate(1.5707963267949,0.46364760900081 , 2.2360679774998);
+        SphericCoordinate correctValue = SphericCoordinate.getOrCreateSphericCoordinate(1.5707963267949,0.46364760900081 , 2.2360679774998);
 
         Assert.assertEquals(correctValue.getPhi(), b.getPhi(), 0.00001);
         Assert.assertEquals(correctValue.getTheta(), b.getTheta(), 0.00001);
         Assert.assertEquals(correctValue.getRadius(), b.getRadius(), 0.00001);
 
     }
-    
+
     @Test
     public void testOriginAsSphericCoordinate(){
-        CartesianCoordinate a = new CartesianCoordinate(0.0, 0.0, 0.0);
+        CartesianCoordinate a = CartesianCoordinate.getOrCreateCartesianCoordinate(0.0, 0.0, 0.0);
         SphericCoordinate b = a.asSphericCoordinate();
-        SphericCoordinate correctValue = new SphericCoordinate(0.0, 0.0, 0.0);
+        SphericCoordinate correctValue = SphericCoordinate.getOrCreateSphericCoordinate(0.0, 0.0, 0.0);
 
         Assert.assertEquals(correctValue.getPhi(), b.getPhi(), 0.0);
         Assert.assertEquals(correctValue.getTheta(), b.getTheta(), 0.0);
         Assert.assertEquals(correctValue.getRadius(), b.getRadius(), 0.0);
 
     }
-    
+
     @Test
     public void testGetCentralAngle(){
-        CartesianCoordinate a = new CartesianCoordinate(1,1,1);
-        CartesianCoordinate b = new CartesianCoordinate( 1,1,1);
+        CartesianCoordinate a = CartesianCoordinate.getOrCreateCartesianCoordinate(1,1,1);
+        CartesianCoordinate b = CartesianCoordinate.getOrCreateCartesianCoordinate( 1,1,1);
 
         double centralAngle = a.getCentralAngle(b);
         double correctValue = 0.0;
@@ -72,26 +72,22 @@ public class CartesianCoordinateTest {
         Assert.assertEquals(correctValue, centralAngle, 0.01);
 
     }
-    
+
     @Test(expected = NullPointerException.class)
     public void testCartesianDistanceNull() throws NullPointerException{
-        CartesianCoordinate a = new CartesianCoordinate(1.0,1.0,1.0);
+        CartesianCoordinate a = CartesianCoordinate.getOrCreateCartesianCoordinate(1.0,1.0,1.0);
         CartesianCoordinate b = null;
         a.getCartesianDistance(b);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testCartesianCoordinateClassInvariant() throws IllegalArgumentException{
-        CartesianCoordinate a = new CartesianCoordinate(Double.NaN, 1.0, 2.0);
+        CartesianCoordinate a = CartesianCoordinate.getOrCreateCartesianCoordinate(Double.NaN, 1.0, 2.0);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testSetInvalidCartesianCoordinate() throws IllegalArgumentException{
-        CartesianCoordinate a = new CartesianCoordinate(1.0,1.0,1.0);
-        a.setxCoordinate(Double.NaN);
-        a.setyCoordinate(Double.NaN);
-        a.setzCoordinate(Double.NaN);
+        CartesianCoordinate a = CartesianCoordinate.getOrCreateCartesianCoordinate(Double.NaN,Double.NaN, Double.NaN);
     }
 
 }
-
