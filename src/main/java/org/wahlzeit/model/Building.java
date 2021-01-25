dbc59b6c9738b7cac794c1121b8653228c3b7043
corinna-hederer <74151356+corinna-hederer@users.noreply.github.com>
2021-01-25 09:03:38 +0100

    adap-cw10
    Update Building.java

--- src/main/java/org/wahlzeit/model/Building.java
+++ src/main/java/org/wahlzeit/model/Building.java
@@ -9,121 +9,62 @@ import java.util.Objects;
 
 public class Building {
 
-    private int completionYear;     //Year, in which building was completed (e.g. '1925')
-    private double coverage;        //constructed area in m² (e.g. 650.8)
-    private String type;            //kind of building (e.g. residence, cinema, hotel, shop, gym,...)
-    private int floor;              //Number of floors (e.g. 3)
-    private boolean privateUse;     //true, if building is a residential/terrace house, apartment building,...
-    private boolean publicUse;      //true, if building is city hall, school, church...
-    private boolean commercialUse;  //true, if building is supermarket, store, office building...
+    private BuildingType buildingType;
+    protected String address;
+
 
 
     /**
-     * Creates a building instance using the arguments completionYear, coverage, type, floor, privateUse, publicUse,
-     * commercialUse
      * @methodtype constructor
-     * @param completionYear Year of completion
-     * @param coverage built surface area
-     * @param type sort of edifice
-     * @param floor number of storeys
-     * @param privateUse only for private usage
-     * @param publicUse publicly accessible
-     * @param commercialUse for commercial usage
      */
-    public Building(int completionYear, double coverage, String type, int floor, boolean privateUse, boolean publicUse,
-                    boolean commercialUse){
-        this.completionYear = completionYear;
-        this.coverage = coverage;
-        this.type = type;
-        this. floor = floor;
-        this. privateUse = privateUse;
-        this.publicUse = publicUse;
-        this. commercialUse = commercialUse;
+    public Building(BuildingType buildingType){
+        this.buildingType = buildingType;
+    }
+
+    public Building(BuildingType buildingType, String address){
+        this.buildingType = buildingType;
+        this.address = address;
     }
 
 
     /**
-     * Getter methods for completionYear, coverage, type, floor, privateUse, publicUse and commercialUse
      * @methodtype get
      */
-    public int getCompletionYear() {
-        return completionYear;
+    public BuildingType getBuildingType() {
+        return buildingType;
     }
-
-    public double getCoverage() {
-        return coverage;
+    public String getAddress(){
+        return address;
     }
-
-    public String getType(){
-        return type;
-    }
-
-    public int getFloor(){
-        return floor;
-    }
-
-    public boolean isPrivateUse() {
-        return privateUse;
-    }
-
-    public boolean isPublicUse() {
-        return publicUse;
-    }
-
-    public boolean isCommercialUse() {
-        return commercialUse;
-    }
-
 
     /**
-     * Setter methods completionYear, coverage, type, floor, privateUse, publicUse and commercialUse
-     * @methodtype set
+      * @methodtype set
      */
-    public void setCompletionYear(int completionYear) {
-        this.completionYear = completionYear;
+      public void setBuildingType(BuildingType buildingType) {
+        this.buildingType = buildingType;
+    }
+    public void setAddress(String address){
+          this.address = address;
     }
 
-    public void setCoverage(double coverage) {
-        this.coverage = coverage;
+
+    public boolean isEqual(Building building){
+          boolean isEqualBuildingType = this.buildingType.equals(building.buildingType);
+          boolean isEqualAddress = this.address.equals(building.address);
+          return isEqualBuildingType && isEqualAddress;
     }
 
-    public void setType(String type) {
-        this.type = type;
-    }
 
-    public void setFloor(int floor) {
-        this.floor = floor;
-    }
-
-    public void setPrivateUse(boolean privateUse) {
-        this.privateUse = privateUse;
-    }
-
-    public void setPublicUse(boolean publicUse) {
-        this.publicUse = publicUse;
-    }
-
-    public void setCommercialUse(boolean commercialUse) {
-        this.commercialUse = commercialUse;
-    }
-    
-        @Override
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Building building = (Building) o;
-        return completionYear == building.completionYear &&
-                Double.compare(building.coverage, coverage) == 0 &&
-                floor == building.floor &&
-                privateUse == building.privateUse &&
-                publicUse == building.publicUse &&
-                commercialUse == building.commercialUse &&
-                Objects.equals(type, building.type);
+        return isEqual(building);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(completionYear, coverage, type, floor, privateUse, publicUse, commercialUse);
+        return Objects.hash(this.buildingType, this.address);
     }
-    
 }
