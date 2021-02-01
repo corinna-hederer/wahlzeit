ebe76d73cd039ab766d93257d6cc865b6590c505
corinna-hederer <74151356+corinna-hederer@users.noreply.github.com>
2021-02-01 11:56:18 +0100

    adap-cw11
    Update Building

--- src/main/java/org/wahlzeit/model/Building.java
+++ src/main/java/org/wahlzeit/model/Building.java
@@ -3,6 +3,23 @@ package org.wahlzeit.model;
 import java.util.Objects;
 
 /**
+ * Documentation of instantiation process
+ *
+ * Sequence of method calls that leads to new object:
+ * 1. BuildingManager::createBuilding -> createBuilding() from BuildingManager is called to initiate object creation
+ * 2. BuildingType::createInstance -> createInstance() from class BuildingType is called to get the type of demanded Building instance
+ * 3. Building::Building -> Building constructor is called, Building object is created
+ *
+ * Object creation as a point in the solution space:
+ * 1. Delegation:       separate-object     -> BuildingManager and BuildingType
+ * 2. Selection:        on-the-spot         -> hard-coded constructor, createInstance() always creates Building instance
+ * 3. Configuration:    in-code             -> no mapping in configuration necessary
+ * 4. Instantiation:    in-code             -> createInstance() calls Building constructor
+ * 5. Initialization:   default             -> fixed field assignment in constructor
+ * 6. Building:         default             -> new object creates dependant object structure
+ */
+
+/**
  * A building is a relatively permanent enclosed construction over a plot of land, having usually a roof and walls,
  * and is used for any of a wide variety of activities
  */
